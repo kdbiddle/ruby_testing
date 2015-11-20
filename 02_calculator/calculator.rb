38c8de9940735578b71adf1b28c7c3297e28f60d
Kevin Biddle <kdbiddle08@gmail.com>
2015-11-19 22:18:08 -0500

    Completed calculator exercise including writing tests for multiply, power, and factorial

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -16,3 +16,37 @@ def sum (arr)
 	end
 	return final
 end
+
+def multiply (arr)
+	if arr == []
+		return 0
+	end
+	product=1
+	arr.each do |i|
+		product *= i
+	end
+	return product
+end
+
+def power(a,b)
+	final=1
+	b.times do
+		final *= a
+	end
+	return final
+end
+
+def factorial(a)
+	if a == 0
+		return 1
+	end
+	value = 1
+	until a == 1 do
+		value *= a
+		a -= 1
+	end
+	return value
+end
+
+
+
