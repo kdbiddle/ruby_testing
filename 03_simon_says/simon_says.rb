22b8704d9bb1e3a5fd34310f7f9e2789d67bd1cf
Kevin Biddle <kdbiddle08@gmail.com>
2015-11-22 23:40:30 -0500

    working on simon says exercise

---
+++ 03_simon_says/simon_says.rb
@@ -0,0 +1,28 @@
+def echo(str)
+	return str
+end
+
+def shout(str)
+	return str.upcase
+end
+
+def repeat(str, *n)
+	if n == []
+		return "#{str} #{str}"
+	else
+		return "#{[str]*n[0]*" "}"
+	end
+end
+
+def start_of_word(str, n)
+	return str.chars.take(n).join
+end
+
+def first_word(str)
+	return str.split[0]
+end
+
+def titleize (str)
+	str = str.chars[0].upcase
+	return str
+end
