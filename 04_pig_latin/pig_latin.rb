6d2210941c68e13054ce6f946f4a74416f569829
Kevin Biddle <kdbiddle08@gmail.com>
2015-11-26 21:30:19 -0500

    Working on pig latin exercise

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,14 +1,18 @@
 def translate(str)
-	firstVowel = /[aeiou]/.match(str)[0]
-	arr = str.chars
-	n = arr.index(firstVowel)
-	if n == 0
-		arr.push("a", "y")
-		return arr.join
-	else 
-		prefixArray = arr.take(n)
-		newArray = arr.drop(n)
-		newArray.push(prefixArray, "a", "y")
-		return newArray.join
+	#splits str into individual words
+	arr = str.split(" ")
+	arr.each do |word|
+		word = word.chars
+		firstVowel = /[aeiou]/.match(str)[0]
+		n = word.index(firstVowel)
+		if n == 0
+			word.push("a", "y")
+			return word.join
+		else 
+			prefixArray = word.take(n)
+			newArray = word.drop(n)
+			newArray.push(prefixArray, "a", "y")
+			return newArray.join
+		end
 	end
 end
