a8be495a28ae683c3005da2f8c4937c20d308414
Kevin Biddle <kdbiddle08@gmail.com>
2015-11-30 23:20:10 -0500

    Completed silly blocks exercise

--- 05_silly_blocks/silly_blocks.rb
+++ 05_silly_blocks/silly_blocks.rb
@@ -1,3 +1,21 @@
 def reverser
 	return yield.split(' ').map{|word| word.reverse}.join(' ')
 end
+
+def adder(*n)
+	if n==[]
+		return yield+1
+	else
+		return yield+3
+	end
+end
+
+def repeater(*n)
+	if n == []
+		yield
+	else
+		n[0].times do |variable|
+			yield
+		end
+	end
+end
