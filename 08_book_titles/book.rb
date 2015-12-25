5b04f376adb2947b3bf97ccc38281c682712fa94
Kevin Biddle <kdbiddle08@gmail.com>
2015-12-25 15:59:41 -0500

    Working on book titles exercise

--- 08_book_titles/book.rb
+++ 08_book_titles/book.rb
@@ -1,4 +1,14 @@
 class Book
-	def title
+	def title(str)
+		arr = str.split
+		arr[0].capitalize!
+		arr.each do |i|
+			if i == "and" || i == "the" || i == "over"
+				nil
+			else
+			i.capitalize!
+			end
+		end
+		return arr.join(" ")
 	end
 end
