be366a5ceb11b1b4f554e9f66ce023e424fd3f3b
Kevin Biddle <kdbiddle08@gmail.com>
2015-12-27 11:31:27 -0500

    Completed book titles exercise

--- 08_book_titles/book.rb
+++ 08_book_titles/book.rb
@@ -1,7 +1,16 @@
 class Book
 	def title=(str)
-		@title = str.capitalize!
+	arr = str.split
+	arr[0].capitalize!
+	arr.each do |i|
+		if i == "and" || i == "the" || i == "a" || i == "an" || i == "of" || i == "in"
+			nil
+		else
+		i.capitalize!
+		end
 	end
+	@title = arr.join(" ")
+end
 
 	def title
 		@title
