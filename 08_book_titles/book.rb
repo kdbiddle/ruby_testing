05907455de93abc1b036a0307c4b247782d85af7
Kevin Biddle <kdbiddle08@gmail.com>
2018-01-23 21:39:34 -0500

    Refactoring and improving comments

--- 08_book_titles/book.rb
+++ 08_book_titles/book.rb
@@ -1,18 +1,23 @@
 class Book
+	#Class variable to hold articles
+	@@articles = ['and', 'the', 'a', 'an', 'of', 'in']
 	
+	#setter
 	def title=(str)
-	arr = str.split
-	arr[0].capitalize!
-	arr.each do |i|
-		if i == "and" || i == "the" || i == "a" || i == "an" || i == "of" || i == "in"
-			nil
-		else
-		i.capitalize!
+		arr = str.split
+		arr[0].capitalize!
+		#For each word, if it's not an article, capitalize it.
+		arr.each do |word|
+			#IF
+			if !@@articles.include?(word)
+				word.capitalize!
+			end
 		end
+		#Re-join the array with spaces
+		@title = arr.join(" ")
 	end
-	@title = arr.join(" ")
-end
 
+	#getter
 	def title
 		@title
 	end
--- 09_timer/timer.rb
+++ 09_timer/timer.rb
@@ -1,16 +1,26 @@
 class Timer
-	#accesses seconds property
+	#getter/setter for self.seconds property
 	attr_accessor :seconds
 
+	#Constructor
 	#initializes seconds to zero and sets instance variable seconds
-	def initialize(seconds=0)
+	def initialize(seconds = 0)
 		@seconds = seconds
 	end
+
+	#Optional getter/setter definitions for seconds. This is handled by the attr_accessor call before the constructor
+	# def seconds=(s)
+	# 	@seconds = s
+	# end
+
+	# def seconds
+	# 	@seconds
+	# end
 
 	#pads single digit numbers with leading zero
 	def padded(time)
 		if time.to_s.length == 1
-			return "0"+time.to_s
+			return "0" + time.to_s
 		else
 			return time.to_s
 		end
@@ -20,7 +30,7 @@ class Timer
 	def time_string
 		hours = padded((@seconds/3600))
 		minutes = padded((@seconds/60)%60)
-		seconds = padded(@seconds%60)
+		seconds = padded(@seconds % 60)
 		return "#{hours}:#{minutes}:#{seconds}"
 	end
 end
